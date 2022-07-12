829a217d9a14cba91d1f10b52521d6bb7c3ebf7c
Bas Schimmel <s1143474@student.windesheim.nl>
2022-07-12 13:23:24 +0200

    Removed Infrastructure Benchmarks

--- tests/Infrastructure.BenchMarks/Program.cs
+++ tests/Infrastructure.BenchMarks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.BenchMarks
 {
@@ -6,7 +7,7 @@ namespace Infrastructure.BenchMarks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
---
+++ tests/Infrastructure.BenchMarks/Services/SpelServiceBenchMark.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Infrastructure.Services;
+
+namespace Infrastructure.BenchMarks.Services
+{
+    public class SpelServiceBenchMark
+    {
+        private readonly SpelService _spelService;
+
+        public SpelServiceBenchMark()
+        {
+            _spelService = new SpelService();
+        }
+    }
+}
