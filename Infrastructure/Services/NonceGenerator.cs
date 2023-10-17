961c638f5c4547fad2ffec1c89ab4dddef53f2ac
Bas Schimmel <s1143474@student.windesheim.nl>
2023-10-17 12:11:48 +0200

    Add Nonce Generator

---
+++ Infrastructure/Services/NonceGenerator.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    internal sealed class NonceGenerator : INonceGenerator
+    {
+        public static Dictionary<string, string> nonces = new Dictionary<string, string>();
+
+        public void CreateNonce(string nonceName)
+        {
+            string nonceValue = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            nonces.Add(nonceName, nonceValue);
+        }
+
+        public string GetNonce(string nonceName)
+        {
+            if (!nonces.ContainsKey(nonceName))
+            {
+                CreateNonce(nonceName);
+            }
+
+            return nonces[nonceName];
+        }
+    }
+}
