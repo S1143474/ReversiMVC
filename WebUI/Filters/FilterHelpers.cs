f062b8c243840cdddd7f615e9dcd93a7322f4c5c
Bas Schimmel <s1143474@student.windesheim.nl>
2023-10-19 20:01:09 +0200

    Add Filter Helper to seperate code

---
+++ WebUI/Filters/FilterHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WebUI.Filters
+{
+    public static class FilterHelpers
+    {
+        /// <summary>
+        /// Check if Speler 1 is waiting on a Speler 1
+        /// </summary>
+        /// <param name="speler2Token"></param>
+        /// <returns></returns>
+        public static bool IsSpeler1Waiting(Guid? speler2Token) => speler2Token is null;
+
+        public static bool IsInWaitingView(object controller, object action) => controller.Equals("Spel") && action.Equals("Waiting");
+
+        /// <summary>
+        /// Check if the Speler is already playing the game
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static bool IsInReversiView(object controller, object action) => controller.Equals("Spel") && action.Equals("Reversi");
+
+        public static bool IsInResultView(object controller, object action) => controller.Equals("Spel") && action.Equals("Result");
+
+    }
+}
--- WebUI/Filters/StillPlayingFilter.cs
+++ WebUI/Filters/StillPlayingFilter.cs
@@ -6,52 +6,10 @@ using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Routing;
+using static WebUI.Filters.FilterHelpers;
 
 namespace WebUI.Filters
 {
-    /*    public class StillPlayingFilter : ActionFilterAttribute
-        {
-            private ISpelService _spelService;
-
-            private SpelJsonModel _spel;
-
-            public StillPlayingFilter([FromServices] ISpelService service)
-            {
-                _spelService = service;
-            }
-
-            public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
-            {
-                var claim = context.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
-
-                if (claim != null)
-                {
-                    _spel = await _spelService.RetrieveSpelViaSpelerToken(claim.Value);
-
-                    if (_spel != null)
-                    {
-                        var routeData = context.RouteData.Values;
-
-                        if (routeData.TryGetValue("controller", out var controller) &&
-                            routeData.TryGetValue("action", out var action))
-                        {
-
-                            if (controller.Equals("Spel") && action.Equals("Details"))
-                            {
-                                await next();
-                                return;
-                            }
-
-                            context.Result = new RedirectResult($"/Spel/Details/{_spel.Token}");
-
-                        }
-
-                    }
-                }
-
-                await next();
-            }
-        }*/
     public class StillPlayingFilter : ResultFilterAttribute
     {
         private ISpelService _spelService;
@@ -98,25 +56,5 @@ namespace WebUI.Filters
 
             await next();
         }
-
-        /// <summary>
-        /// Check if Speler 1 is waiting on a Speler 1
-        /// </summary>
-        /// <param name="speler2Token"></param>
-        /// <returns></returns>
-        private bool IsSpeler1Waiting(Guid? speler2Token) => speler2Token is null;
-
-        private bool IsInWaitingView(object controller, object action) => controller.Equals("Spel") && action.Equals("Waiting");
-
-        /// <summary>
-        /// Check if the Speler is already playing the game
-        /// </summary>
-        /// <param name="controller"></param>
-        /// <param name="action"></param>
-        /// <returns></returns>
-        private bool IsInReversiView(object controller, object action) => controller.Equals("Spel") && action.Equals("Reversi");
-
-        private bool IsInResultView(object controller, object action) => controller.Equals("Spel") && action.Equals("Result");
-
     }
 }
